38823d1e08326289f7721f46ca5e21299525b3cb
Beau Gunderson <beau@beaugunderson.com>
2020-09-18 17:56:05 -0700

    fix tests

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
+set -e
+
+npx tsc
+
 ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha \
   -- -R spec && \
-  cat ./coverage/coverage.json |\
-  ./node_modules/codecov.io/bin/codecov.io.js && \
+  ./node_modules/codecov.io/bin/codecov.io.js < ./coverage/coverage.json && \
   rm -rf .coverage && \
   ./node_modules/.bin/mochify -R spec
