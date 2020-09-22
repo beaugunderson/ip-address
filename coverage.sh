892836384a9bde6b48731b50e41ac03152c5316b
Beau Gunderson <beau@beaugunderson.com>
2020-09-22 00:05:58 -0700

    fix coverage reporting

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+set -e
+
+mkdir -p coverage
+
 ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha \
   -- --require ts-node/register --inline-diff --reporter spec test/**/*.ts && \
   ./node_modules/codecov.io/bin/codecov.io.js < ./coverage/coverage.json && \
