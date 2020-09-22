56e4470a2d67939037187047588d439cbf0a1859
Beau Gunderson <beau@beaugunderson.com>
2020-09-22 00:33:35 -0700

    actually fix coverage

--- coverage.sh
+++ coverage.sh
@@ -4,8 +4,6 @@ set -e
 
 mkdir -p coverage
 
-./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha \
-  -- --require ts-node/register --inline-diff --reporter spec test/**/*.ts && \
-  ./node_modules/codecov.io/bin/codecov.io.js < ./coverage/coverage.json && \
-  rm -rf .coverage && \
-  ./node_modules/.bin/mochify -R spec
+npm run test-ci
+
+npx codecov
