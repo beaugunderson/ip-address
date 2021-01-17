742410ef83a966d1641070353604576baf1c8a95
Nathaniel Peiffer <nathaniel@peiffer.com.au>
2021-01-17 20:51:24 +1100

    Revert "use jest for testing (fix ci)"
    This reverts commit 1bcc8ac63b21d3ebc41af9ddb3cbcfc202417c22.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.(t|j)sx?$': 'ts-jest',
-  },
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverage: true,
-};
