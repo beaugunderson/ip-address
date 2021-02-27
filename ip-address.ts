554f865f24cbf31c7a1634042c57e821c697d3a7
Alberto Restifo <alberto@restifo.dev>
2021-02-27 12:15:53 +0100

    Fix require path

--- ip-address.ts
+++ ip-address.ts
@@ -1,6 +1,6 @@
 import { Address4 } from './lib/ipv4';
 import { Address6 } from './lib/ipv6';
-import { AddressError } from "./lib/address-error';
+import { AddressError } from './lib/address-error';
 
 export { Address4 };
 export { Address6 };
